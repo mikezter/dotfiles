4c09df02d5fa44491d9fb6bbaea3fa2317342ff8
Michael Strüder <ms@kaeuferportal.de>
2016-03-01 13:53:05 +0100

    extract layouts

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,4 +1,4 @@
-local awful = require("awful")
+awful = require("awful")
 awful.rules = require("awful.rules")
 require("awful.autofocus")
 local wibox = require("wibox")
@@ -34,30 +34,11 @@ end
 beautiful.init("/home/mike/.config/awesome/theme.lua")
 awful.util.spawn("feh --bg-max -z -r " .. beautiful.wallpaper_dir)
 
-toto_layout = require("toto_layout")
-two_thirds_layout = require("two_thirds_layout")
--- Table of layouts to cover with awful.layout.inc, order matters.
-local layouts =
-{
-    two_thirds_layout,
-    toto_layout,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.floating,
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier
-}
--- }}}
 
 terminal = "terminator"
 modkey   = "Mod4"
+
+layouts = require("layouts")
 
 tags = {}
 for s = 1, screen.count() do
